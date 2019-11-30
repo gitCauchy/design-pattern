8f83c97ae2029769d22ea4b6c2dbb5c8bd91e1b8
gitCauchy <mailcauchy@gmail.com>
2019-11-30 22:13:04 +0800

    享元模式整理完成

--- src/com/cauchy/struct/flyweight/Bullet.java
+++ src/com/cauchy/struct/flyweight/Bullet.java
@@ -1,11 +1,19 @@
 package com.cauchy.struct.flyweight;
 
 import java.util.UUID;
-
+/**
+ * 
+ * @author Cauchy
+ * @ClassName Bullet.java
+ * @Date 2019年11月30日
+ * @Description 享元模式-实体类-子弹
+ * @Version 
+ *
+ */
 public class Bullet {
 	public UUID id = UUID.randomUUID();
 	boolean living = true;
-	@Override
+
 	public String toString() {
 		return "Bullet [id=" + id + "]";
 	}
--- src/com/cauchy/struct/flyweight/BulletPool.java
+++ src/com/cauchy/struct/flyweight/BulletPool.java
@@ -3,28 +3,39 @@ package com.cauchy.struct.flyweight;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ * @author Cauchy
+ * @ClassName BulletPool.java
+ * @Date 2019年11月30日
+ * @Description 享元模式-将小对象放置在池子中，使用时无需new，直接从池子里面取
+ * @Version
+ *
+ */
 public class BulletPool {
 	List<Bullet> bullets = new ArrayList<Bullet>();
 	{
-		for(int i = 0;i < 5; i++) {
+		for (int i = 0; i < 500; i++) {
 			bullets.add(new Bullet());
 		}
 	}
+
 	public Bullet getBullet() {
-		for(int i = 0; i < bullets.size(); i ++) {
+		for (int i = 0; i < bullets.size(); i++) {
 			Bullet b = bullets.get(i);
-			if(!b.living) {
+			if (!b.living) {
 				return b;
 			}
 		}
 		return new Bullet();
 	}
+
 	public static void main(String[] args) {
 		BulletPool bp = new BulletPool();
-		for(int i = 0 ; i < 10 ;i ++) {
+		for (int i = 0; i < 1000; i++) {
 			Bullet bullet = bp.getBullet();
 			System.out.println(bullet);
 		}
 	}
-	
+
 }
