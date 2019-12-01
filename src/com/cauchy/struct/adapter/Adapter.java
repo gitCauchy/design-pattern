945f3d65ae9080ce4e6675c5e2ec883bfb3d0618
gitCauchy <mailcauchy@gmail.com>
2019-12-01 11:01:17 +0800

    Adapter模式整理完成

---
+++ src/com/cauchy/struct/adapter/Adapter.java
@@ -0,0 +1,30 @@
+package com.cauchy.struct.adapter;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ * @author Cauchy
+ * @ClassName Adapter.java
+ * @Date 2019年12月1日
+ * @Description 适配器模式，当一个类不能直接访问另一个类，需要添加一个转换器来达到目的。
+ * @Version
+ *
+ */
+public class Adapter {
+	
+	public static final String FILE = "";
+	
+	public static void main(String[] args) throws Exception {
+		FileInputStream fis = new FileInputStream(FILE);
+		InputStreamReader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		String line = br.readLine();
+		while (line != null) {
+			System.out.println(line);
+		}
+		br.close();
+	}
+}
