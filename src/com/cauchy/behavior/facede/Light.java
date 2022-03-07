236b39229a1a1894ebb0e23ae208b8649fdce46e
Cauchy <mailcauchy@gmail.com>
2022-03-07 10:55:34 +0800

    外观模式

--- src/com/cauchy/behavior/facede/Light.java
+++ src/com/cauchy/behavior/facede/Light.java
@@ -1,15 +1,15 @@
 package com.cauchy.behavior.facede;
 
 public class Light {
-	public void turnOff() {
-		System.out.println("turn off the light");
-	}
-	
-	public void turnOn() {
-		System.out.println("turn on the light");
-	}
-	
-	public void changeBulb() {
-		System.out.println("change the bulb");
-	}
+    public void turnOff() {
+        System.out.println("turn off the light");
+    }
+
+    public void turnOn() {
+        System.out.println("turn on the light");
+    }
+
+    public void changeBulb() {
+        System.out.println("change the bulb");
+    }
 }
--- src/com/cauchy/behavior/facede/SecurityFacade.java
+++ src/com/cauchy/behavior/facede/SecurityFacade.java
@@ -2,30 +2,30 @@ package com.cauchy.behavior.facede;
 
 public class SecurityFacade {
 
-	private static Camera camera1, camera2;
-	private static Light light1, light2, light3;
+    private static Camera camera1, camera2;
+    private static Light light1, light2, light3;
 
-	SecurityFacade() {
-		camera1 = new Camera();
-		camera2 = new Camera();
-		light1 = new Light();
-		light2 = new Light();
-		light3 = new Light();
-	}
+    SecurityFacade() {
+        camera1 = new Camera();
+        camera2 = new Camera();
+        light1 = new Light();
+        light2 = new Light();
+        light3 = new Light();
+    }
 
-	public void activate() {
-		camera1.turnOn();
-		camera2.turnOn();
-		light1.turnOn();
-		light2.turnOn();
-		light3.turnOn();
-	}
+    public void activate() {
+        camera1.turnOn();
+        camera2.turnOn();
+        light1.turnOn();
+        light2.turnOn();
+        light3.turnOn();
+    }
 
-	public void deactivate() {
-		camera1.turnOff();
-		camera2.turnOff();
-		light1.turnOff();
-		light2.turnOff();
-		light3.turnOff();
-	}
+    public void deactivate() {
+        camera1.turnOff();
+        camera2.turnOff();
+        light1.turnOff();
+        light2.turnOff();
+        light3.turnOff();
+    }
 }
